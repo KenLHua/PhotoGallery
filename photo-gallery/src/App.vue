131b1991f575061e382042783d81769842e98a33
Ken <khuarice@gmail.com>
2021-01-30 22:23:52 -0800

    gallery click implemented

--- photo-gallery/src/App.vue
+++ photo-gallery/src/App.vue
@@ -1,50 +1,13 @@
 <template>
-  <Gallery />
+  <router-view />
 </template>
-<script>
-  import Gallery from '@/components/Gallery.vue';
-  export default {
-    name: 'home',
-    components: {
-      Gallery,
-    }
-  };
-</script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Roboto');
+  html { font-size: 22px; }
+  body { font-family: 'Roboto', sans-serif; }
   a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+    color: #0094FF;
+    text-decoration: none;
   }
-}
-.gallery {
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
-  grid-gap: 1rem;
-  max-width: 80rem;
-  margin: 5rem auto;
-  padding: 0 5rem;
-}
-.gallery-panel img {
-  width: 100%;
-  height: 22vw;
-  object-fit: cover;
-  border-radius: 0.75rem;
-}
-
+  a:hover { color: #0074C6; }
 </style>
